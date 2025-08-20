4802e72aee6a5109e562358f0c3b8a7a7e20c806
anmols97 <anmols97@yahoo.co.in>
2025-08-20 01:02:43 -0700

    Remove unused Hero component and update imports
    Delete unused Hero.tsx and update Home.tsx to use HeroSimple.
    Consolidate to single hero implementation.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import { Layout } from '../components/Layout'
-import { Hero } from '../components/Hero'
+import { HeroSimple } from '../components/HeroSimple'
 import { LogoCloud } from '../components/LogoCloud'
 import { FeatureSection } from '../components/FeatureSection'
 import { CTASection } from '../components/CTASection'
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Layout>
       <Seo title="Home" path="/" />
-      <Hero />
+      <HeroSimple />
       <LogoCloud />
       <FeatureSection />
       <CTASection />
