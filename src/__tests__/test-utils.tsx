59a07fb602edc33098e25e84917994a1f36fc561
anmols97 <anmols97@yahoo.co.in>
2025-08-28 00:59:49 -0700

    Fix test utilities
    Add ThemeProvider to test utilities to support components that need theme context

--- src/__tests__/test-utils.tsx
+++ src/__tests__/test-utils.tsx
@@ -3,12 +3,15 @@ import { render } from '@testing-library/react'
 import { IntlProvider } from 'react-intl'
 import { MemoryRouter } from 'react-router-dom'
 import { messages } from '../i18n'
+import { ThemeProvider } from '../components/ThemeProvider'
 
 // Custom render function with providers
 export function renderWithProviders(ui: ReactElement) {
   return render(
     <IntlProvider locale="en" messages={messages.en}>
-      <MemoryRouter>{ui}</MemoryRouter>
+      <ThemeProvider>
+        <MemoryRouter>{ui}</MemoryRouter>
+      </ThemeProvider>
     </IntlProvider>
   )
 }
